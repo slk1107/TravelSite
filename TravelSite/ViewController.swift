94acad18ba8c44ad9a61a6dfa6fd3f744f988da5
Kris <>
2020-02-15 23:19:14 +0800

    Start new project

---
+++ TravelSite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelSite
+//
+//  Created by Kris on 2020/2/15.
+//  Copyright © 2020 Kris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TravelSiteTests/TravelSiteTests.swift
@@ -0,0 +1,34 @@
+//
+//  TravelSiteTests.swift
+//  TravelSiteTests
+//
+//  Created by Kris on 2020/2/15.
+//  Copyright © 2020 Kris. All rights reserved.
+//
+
+import XCTest
+@testable import TravelSite
+
+class TravelSiteTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
